443a5027ce3c198fd1824bce2a76bfd73d8cba21
Miguel Leite <miguelleite200leite@gmal.com>
2023-02-01 10:57:38 +0100

    💄 UI: users page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,119 @@
+
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+
+import { Header } from "@/src/components/Header";
+import { Sidebar } from "@/src/components/Sidebar";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+
+export default function Users() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="purple"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="purple" />
+                </Th>
+                <Th> Usuário </Th>
+                <Th> Data de cadastro </Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="purple" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Miguel Leite</Text>
+                    <Text fontSize="sm" color="gray.300">miguelleite200leite@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>01 de Fevereiro, 2023</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="blue"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="purple" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Miguel Leite</Text>
+                    <Text fontSize="sm" color="gray.300">miguelleite200leite@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>01 de Fevereiro, 2023</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="blue"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="purple" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Miguel Leite</Text>
+                    <Text fontSize="sm" color="gray.300">miguelleite200leite@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>01 de Fevereiro, 2023</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="blue"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
