3065dda3c4c9081fc13d34fe1ea73fc00b3f5e57
Miguel Leite <miguelleite200leite@gmal.com>
2023-02-01 11:53:51 +0100

    💄 UI: component edited

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -8,7 +8,7 @@ interface InputProps extends ChakraInputProps {
 export function Input({ name, label, ...rest } : InputProps) {
   return (
     <FormControl>
-      { !!label && <FormLabel htmlFor={name}>E-mail</FormLabel>}
+      { !!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
       <ChakraInput
         name={name}
         type={name}
